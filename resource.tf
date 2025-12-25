6f8c347c3d59274017dfb65058b7e9c21f54d2f9
ankit <abc@gmail.com>
2025-12-25 23:24:34 +0530

    change3

--- resource.tf
+++ resource.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "name_instance1" {
   ami               = "ami-02b8269d5e85954ef"
   instance_type     = "t3.micro"
-  key_name          = " key1"
+  key_name          = "key1"
   availability_zone = "ap-south-1a"
 
 
