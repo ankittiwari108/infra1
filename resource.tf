94cb45231e8803e8e189acdde69b111d602ffd30
ankit <abc@gmail.com>
2025-12-25 20:09:37 +0530

    commit for jenkins

---
+++ resource.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "name_instance1" {
+  ami               = "ami-02b8269d5e85954ef"
+  instance_type     = "t3.micro"
+  key_name          = " key1"
+  availability_zone = "ap-south-1a"
+
+
+}
